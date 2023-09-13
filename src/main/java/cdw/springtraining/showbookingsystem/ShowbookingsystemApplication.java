f976543bdb4238398232297fa23e9a8ef2caa5f2
thisIsMaamathi <125466717+thisIsMaamathi@users.noreply.github.com>
2023-09-13 17:30:59 +0530

    initialCommit

---
+++ src/main/java/cdw/springtraining/showbookingsystem/ShowbookingsystemApplication.java
@@ -0,0 +1,13 @@
+package cdw.springtraining.showbookingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowbookingsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowbookingsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cdw/springtraining/showbookingsystem/ShowbookingsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package cdw.springtraining.showbookingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowbookingsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
